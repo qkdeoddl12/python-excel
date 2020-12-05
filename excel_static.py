9833b9690943e7c0da878b9b5cb89af6d249cc1f
DESKTOP-JRJFT0O\Eric-woo <qkdeoddl12@naver.com>
2020-12-05 22:49:08 +0900

    asd

---
+++ excel_static.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df=pd.read_excel("C:\WORK\python\data.xlsx",sheet_name='11월판매데이터')
+#print(df)
+
+grouped=df.groupby(by=['품번'],as_index=False).sum()
+grouped.to_excel('text.xlsx',sheet_name='test1')
+print(grouped)
