ab6620126067880605403b010da80c5f47717872
DESKTOP-JRJFT0O\Eric-woo <qkdeoddl12@naver.com>
2020-12-05 22:03:10 +0900

    no message

---
+++ import.py
@@ -0,0 +1,21 @@
+from openpyxl import load_workbook
+
+load_wb=load_workbook("C:\WORK\python\과일.xlsx",data_only=True)
+
+load_ws=load_wb['Sheet1']
+
+print(load_ws['A1'].value)
+
+print(load_ws.cell(1,2).value)
+
+print('\n------지정한 셀 출력-----')
+get_cells=load_ws['A1':'D2']
+for row in get_cells:
+    for cell in row:
+        print(cell.value)
+
+
+print('\n------모든 행 단위로 출력-----')
+
+for row in load_ws.rows:
+        print(row)
